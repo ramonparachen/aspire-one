8b00da6012fac5a1798df50ea89ee476586813be
Ramon <ramonparachen@gmail.com>
2022-08-05 14:50:38 -0300

    Added jgmenu with "temporary" method of installation

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 sudo apt install openbox obconf idesk tint2 network-manager network-manager-gnome cbatticon xterm lightdm
+echo "deb http://ftp.de.debian.org/debian bookworm main" | sudo tee -a /etc/apt/sources.list
+sudo apt update
+sudo apt install jgmenu
+sudo sed -i '$d' /etc/apt/sources.list
+
 cp /usr/share/idesk/dot.ideskrc ~/.ideskrc
 mkdir ~/.idesktop
 

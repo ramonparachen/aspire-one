e7d81b59d972c72da026d5d2d86b70b157c4239f
Ramon <ramonparachen@gmail.com>
2022-08-08 17:10:22 -0300

    Removed everything related to idesk

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
-sudo apt install openbox obconf idesk tint2 network-manager network-manager-gnome cbatticon xterm lightdm
+sudo apt install openbox obconf tint2 network-manager network-manager-gnome cbatticon xterm lightdm
 echo "deb http://ftp.de.debian.org/debian bookworm main" | sudo tee -a /etc/apt/sources.list
 sudo apt update
 sudo apt install jgmenu
 sudo sed -i '$d' /etc/apt/sources.list
-
-cp /usr/share/idesk/dot.ideskrc ~/.ideskrc
-mkdir ~/.idesktop
 
 mkdir ~/.config/openbox
 cp autostart.sh ~/.config/openbox/
